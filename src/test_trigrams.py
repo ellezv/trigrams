c463f70938e701f76ae67f18b503e092da37429a
Colin Lamont <colinlamont@gmail.com>
2016-12-07 14:17:36 -0800

    added 2nd test to assert list of text gets created

--- src/test_trigrams.py
+++ src/test_trigrams.py
@@ -5,3 +5,9 @@ def test_trigrams():
     """Test that the two_word function returns a two-word string."""
     from trigrams import two_words
     assert two_words().count(" ") == 1
+
+
+def test_pulltext():
+    """Test pulling text from a file and storing it as a string."""
+    from trigrams import pulltext
+    assert len(pulltext())
