1634100ed906f55ac69d11d93a6d064158f198ca
Maelle Vance <maellevance@gmail.com>
2016-12-07 14:08:41 -0800

    simple function to return the first two words as a string

--- src/trigrams.py
+++ src/trigrams.py
@@ -1,1 +1,8 @@
 """This module contains an implementation of a trigram algorithm."""
+
+BOOK_LIST = ['I', 'wish', 'I', 'may', 'I', 'wish', 'I', 'might']
+
+
+def two_words():
+    """Take a list and return a string of the first two indices."""
+    return str(BOOK_LIST[0:2])
