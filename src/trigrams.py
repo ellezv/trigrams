17cd5b93b8a8b15c7f9e21d9e00c07efb7009a23
Maelle Vance <maellevance@gmail.com>
2016-12-07 15:12:20 -0800

    added import io

--- src/trigrams.py
+++ src/trigrams.py
@@ -1,8 +1,12 @@
 """This module contains an implementation of a trigram algorithm."""
 
-BOOK_LIST = ['I', 'wish', 'I', 'may', 'I', 'wish', 'I', 'might']
+import io
 
 
 def two_words():
     """Take a list and return a string of the first two indices."""
     return str(BOOK_LIST[0:2])
+
+
+def pulltext():
+    text = io.open("../sample.txt")
