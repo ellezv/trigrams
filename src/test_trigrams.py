8e80950a754f3978908b34786523e410f66021ac
Colin Lamont <colinlamont@gmail.com>
2016-12-07 13:52:30 -0800

    initial base test to see if we produce two words

--- src/test_trigrams.py
+++ src/test_trigrams.py
@@ -1,1 +1,5 @@
 """This module contains tests for our trigrams module."""
+def test_trigrams(n)
+    """Test that the two_word function returns a two-word string"""
+    from trigrams import two_words
+    assert two_words().count(" ") == 1
