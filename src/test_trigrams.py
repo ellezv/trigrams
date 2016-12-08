e7ff5c2b16a58c78384facfedd72ff762ccd39d0
Colin Lamont <colinlamont@gmail.com>
2016-12-07 18:38:47 -0800

    added test to see if output file is the same as the function produced

--- src/test_trigrams.py
+++ src/test_trigrams.py
@@ -9,6 +9,7 @@ DICT_TABLE = [
     ["was on", ["the"]],
 ]
 
+OUTPUT_LIST = ["up to the chamber which had formerly been in part my own"]
 
 def test_pulltext():
     """Test pulling text and splitting it into a list of single words."""
@@ -21,3 +22,9 @@ def test_build_tridict(key, value):
     """Test build_tridict function to build dictionary."""
     from trigrams import build_tridict
     assert build_tridict()[key] == value
+
+
+def test_create_output():
+    """Test create_output function which builds text off last 2 words in output file."""
+    from trigrams import create_output
+    assert create_output(10) == OUTPUT_LIST
