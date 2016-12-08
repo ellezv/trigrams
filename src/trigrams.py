84be70997fd547efc8f55e9d785d1ec6b06b6fe8
Colin Lamont <colinlamont@gmail.com>
2016-12-07 17:00:21 -0800

    built enumerator that builds out the 2-word keys and value pairs

--- src/trigrams.py
+++ src/trigrams.py
@@ -6,3 +6,15 @@ import io
 def pulltext():
     """Open a text file and turn it into a list of single word strings."""
     return io.open("../sample.txt").read().split()
+
+
+def build_tridict():
+    """Enumerate through file to build 2-word keys and the value pairs."""
+    tridict = {}
+    text = pulltext()
+    for idx, item in enumerate(text):
+        try:
+            tridict.setdefault(text[idx] + " " + text[idx + 1], []).append(text[idx + 2])
+        except:
+            break
+    return tridict
